81c93b4f04993f42f80dfeaaff12d3da2e9ef73a
Андрій <andreww@ukr.net>
2019-08-07 11:45:28 +0300

    Lesson04 Exceptions

---
+++ src/javawebinar/basejava/exception/ExistStorageException.java
@@ -0,0 +1,7 @@
+package javawebinar.basejava.exception;
+
+public class ExistStorageException extends StorageException {
+    public ExistStorageException(String uuid) {
+        super("Resume " + uuid + " already exist", uuid);
+    }
+}
