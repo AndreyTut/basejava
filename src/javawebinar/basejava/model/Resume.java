3f04ba0b8d3784a990719baff72092882aa7aa18
Андрій <andreww@ukr.net>
2019-08-19 14:39:02 +0300

    HW6 Resume equals&hash with fullName

--- src/javawebinar/basejava/model/Resume.java
+++ src/javawebinar/basejava/model/Resume.java
@@ -1,5 +1,6 @@
 package javawebinar.basejava.model;
 
+import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -16,6 +17,8 @@ public class Resume {
     }
 
     public Resume(String uuid, String fullName) {
+        Objects.requireNonNull(fullName, "fullName must not be null");
+        Objects.requireNonNull(uuid, "uuid must not be null");
         this.uuid = uuid;
         this.fullName = fullName;
     }
@@ -31,20 +34,14 @@ public class Resume {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
+        if (!(o instanceof Resume)) return false;
         Resume resume = (Resume) o;
-
-        return uuid.equals(resume.uuid);
+        return getUuid().equals(resume.getUuid()) &&
+                getFullName().equals(resume.getFullName());
     }
 
     @Override
     public int hashCode() {
-        return uuid.hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return uuid;
+        return Objects.hash(getUuid(), getFullName());
     }
 }
