c9b2b711e1454ff538cbf5b1f12f68f6748492fc
Андрій <andreww@ukr.net>
2019-09-29 00:05:23 +0300

    HW10 functional interface

--- src/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
+++ src/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
@@ -17,88 +17,73 @@ public class DataStreamSerializer implements StreamSerializer {
         try (DataOutputStream dataOutputStream = new DataOutputStream(outputStream)) {
             dataOutputStream.writeUTF(resume.getUuid());
             dataOutputStream.writeUTF(resume.getFullName());
-            Map<ContactType, String> contacts = resume.getContacts();
-            dataOutputStream.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
-                dataOutputStream.writeUTF(entry.getKey().name());
-                dataOutputStream.writeUTF(entry.getValue());
-            }
+
+            writeMap(resume.getContacts(), dataOutputStream, (t, s) -> dataOutputStream.writeUTF(s));
 
             Map<SectionType, AbstractSection> sections = resume.getSections();
 
-            dataOutputStream.writeInt(sections.size());
-            for (Map.Entry<SectionType, AbstractSection> entry : sections.entrySet()) {
-                switch (entry.getKey()) {
+            writeMap(sections, dataOutputStream, (t, s) -> {
+                switch ((SectionType) t) {
                     case PERSONAL:
-                        writeTextSection(dataOutputStream, sections, PERSONAL);
-                        break;
                     case OBJECTIVE:
-                        writeTextSection(dataOutputStream, sections, OBJECTIVE);
+                        writeTextSection(dataOutputStream, sections, (SectionType) t);
                         break;
                     case ACHIEVEMENT:
-                        writeListSection(dataOutputStream, sections, ACHIEVEMENT);
-                        break;
                     case QUALIFICATIONS:
-                        writeListSection(dataOutputStream, sections, QUALIFICATIONS);
+                        writeListSection(dataOutputStream, sections, (SectionType) t);
                         break;
                     case EXPERIENCE:
-                        writeOrganizationSection(dataOutputStream, sections, EXPERIENCE);
-                        break;
                     case EDUCATION:
-                        writeOrganizationSection(dataOutputStream, sections, EDUCATION);
-                        break;
+                        writeOrganizationSection(dataOutputStream, sections, (SectionType) t);
                 }
-            }
+            });
         }
     }
 
     private void writeTextSection(DataOutputStream dataOutputStream, Map<SectionType, AbstractSection> sections, SectionType type) throws IOException {
-        dataOutputStream.writeUTF(type.getTitle());
         TextSection textSection = (TextSection) sections.get(type);
-            dataOutputStream.writeUTF(textSection.getContent());
+        dataOutputStream.writeUTF(textSection.getContent());
     }
 
     private void writeListSection(DataOutputStream dataOutputStream, Map<SectionType, AbstractSection> sections, SectionType type) throws IOException {
         ListSection listSection = (ListSection) sections.get(type);
-        dataOutputStream.writeUTF(type.getTitle());
-            List<String> items = listSection.getItems();
-            dataOutputStream.writeInt(items.size());
-            for (String item : items) {
-                dataOutputStream.writeUTF(item);
-            }
+        List<String> items = listSection.getItems();
+        dataOutputStream.writeInt(items.size());
+        for (String item : items) {
+            dataOutputStream.writeUTF(item);
+        }
     }
 
     private void writeOrganizationSection(DataOutputStream dataOutputStream, Map<SectionType, AbstractSection> sections, SectionType type) throws IOException {
         OrganizationSection organizationSection = (OrganizationSection) sections.get(type);
-        dataOutputStream.writeUTF(type.getTitle());
         List<Organization> organizations = organizationSection.getOrganizations();
-            dataOutputStream.writeInt(organizations.size());
+        dataOutputStream.writeInt(organizations.size());
 
-            for (Organization organization : organizations) {
-                Link homePage = organization.getHomePage();
-                dataOutputStream.writeUTF(homePage.getName());
-                String url = homePage.getUrl();
-                if (url != null) {
-                    dataOutputStream.writeUTF(url);
+        for (Organization organization : organizations) {
+            Link homePage = organization.getHomePage();
+            dataOutputStream.writeUTF(homePage.getName());
+            String url = homePage.getUrl();
+            if (url != null) {
+                dataOutputStream.writeUTF(url);
+            } else {
+                dataOutputStream.writeUTF("");
+            }
+
+            List<Organization.Position> positions = organization.getPositions();
+            dataOutputStream.writeInt(positions.size());
+
+            for (Organization.Position position : positions) {
+                dataOutputStream.writeUTF(position.getStartDate().toString());
+                dataOutputStream.writeUTF(position.getEndDate().toString());
+                dataOutputStream.writeUTF(position.getTitle());
+                String description = position.getDescription();
+                if (description != null) {
+                    dataOutputStream.writeUTF(description);
                 } else {
                     dataOutputStream.writeUTF("");
                 }
-
-                List<Organization.Position> positions = organization.getPositions();
-                dataOutputStream.writeInt(positions.size());
-
-                for (Organization.Position position : positions) {
-                    dataOutputStream.writeUTF(position.getStartDate().toString());
-                    dataOutputStream.writeUTF(position.getEndDate().toString());
-                    dataOutputStream.writeUTF(position.getTitle());
-                    String description = position.getDescription();
-                    if (description != null) {
-                        dataOutputStream.writeUTF(description);
-                    } else {
-                        dataOutputStream.writeUTF("");
-                    }
-                }
             }
+        }
     }
 
     @Override
@@ -118,22 +103,22 @@ public class DataStreamSerializer implements StreamSerializer {
                 String type = dataInputStream.readUTF();
 
                 switch (type) {
-                    case "Личные качества":
+                    case "PERSONAL":
                         resume.addSection(PERSONAL, new TextSection(dataInputStream.readUTF()));
                         break;
-                    case "Позиция":
+                    case "OBJECTIVE":
                         resume.addSection(OBJECTIVE, new TextSection(dataInputStream.readUTF()));
                         break;
-                    case "Достижения":
+                    case "ACHIEVEMENT":
                         resume.addSection(ACHIEVEMENT, new ListSection(readListSection(dataInputStream)));
                         break;
-                    case "Квалификация":
+                    case "QUALIFICATIONS":
                         resume.addSection(QUALIFICATIONS, new ListSection(readListSection(dataInputStream)));
                         break;
-                    case "Опыт работы":
+                    case "EXPERIENCE":
                         resume.addSection(EXPERIENCE, new OrganizationSection(readOrganizationSection(dataInputStream)));
                         break;
-                    case "Образование":
+                    case "EDUCATION":
                         resume.addSection(EDUCATION, new OrganizationSection(readOrganizationSection(dataInputStream)));
                         break;
                 }
@@ -179,5 +164,13 @@ public class DataStreamSerializer implements StreamSerializer {
             organizations.add(organization);
         }
         return organizations;
+    }
+
+    private <T> void writeMap(Map<? extends Enum, T> map, DataOutputStream dataOutputStream, MapWriter<T> writer) throws IOException {
+        dataOutputStream.writeInt(map.size());
+        for (Map.Entry<? extends Enum, T> entry : map.entrySet()) {
+            dataOutputStream.writeUTF(entry.getKey().name());
+            writer.write(entry.getKey(), entry.getValue());
+        }
     }
 }
