18f6102be0fabc805d77f05a11d15fc02797e7f4
Андрій <andreww@ukr.net>
2019-10-30 15:41:47 +0200

    HW13 process ExistStorageException in sqlHelper

--- src/javawebinar/basejava/storage/SqlStorage.java
+++ src/javawebinar/basejava/storage/SqlStorage.java
@@ -1,10 +1,10 @@
 package javawebinar.basejava.storage;
 
+import javawebinar.basejava.Config;
 import javawebinar.basejava.exception.ExistStorageException;
 import javawebinar.basejava.exception.NotExistStorageException;
 import javawebinar.basejava.exception.StorageException;
 import javawebinar.basejava.model.Resume;
-import javawebinar.basejava.sql.ConnectionFactory;
 import javawebinar.basejava.util.SqlHelper;
 
 import java.sql.DriverManager;
@@ -13,17 +13,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SqlStorage implements Storage {
-    private final ConnectionFactory connectionFactory;
     private SqlHelper helper;
 
     public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
-        connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
-        helper = new SqlHelper();
+        helper = new SqlHelper(() -> DriverManager.getConnection(dbUrl, dbUser, dbPassword));
     }
 
     @Override
     public void clear() {
-        helper.executeStatement(connectionFactory, "DELETE FROM resume", statement -> {
+        helper.executeStatement("DELETE FROM resume", statement -> {
             statement.execute();
             return null;
         });
@@ -31,29 +29,27 @@ public class SqlStorage implements Storage {
 
     @Override
     public void update(Resume resume) {
-        delete(resume.getUuid());
-        save(resume);
+        helper.executeStatement("UPDATE resume SET full_name=? WHERE uuid=?", statement -> {
+            statement.setString(1, resume.getFullName());
+            statement.setString(2, resume.getUuid());
+            statement.execute();
+            return null;
+        });
     }
 
     @Override
     public void save(Resume resume) {
-        try {
-            helper.executeStatement(connectionFactory, "INSERT INTO resume (uuid, full_name) VALUES (?,?)", statement -> {
+            helper.executeStatement("INSERT INTO resume (uuid, full_name) VALUES (?,?)", statement -> {
                 statement.setString(1, resume.getUuid());
                 statement.setString(2, resume.getFullName());
                 statement.execute();
                 return null;
             });
-        } catch (StorageException e) {
-            if (e.getMessage().contains("duplicate key value")) {
-                throw new ExistStorageException(resume.getUuid());
-            }
-        }
     }
 
     @Override
     public Resume get(String uuid) {
-        return helper.executeStatement(connectionFactory, "SELECT * FROM resume r WHERE r.uuid =?", statement -> {
+        return helper.executeStatement("SELECT * FROM resume r WHERE r.uuid =?", statement -> {
             statement.setString(1, uuid);
             ResultSet resultSet = statement.executeQuery();
             if (!resultSet.next()) {
@@ -65,7 +61,7 @@ public class SqlStorage implements Storage {
 
     @Override
     public void delete(String uuid) {
-        helper.executeStatement(connectionFactory, "DELETE FROM resume WHERE uuid=?", statement -> {
+        helper.executeStatement("DELETE FROM resume WHERE uuid=?", statement -> {
             statement.setString(1, uuid);
             if (statement.executeUpdate() == 0) {
                 throw new NotExistStorageException(uuid);
@@ -78,7 +74,7 @@ public class SqlStorage implements Storage {
 
     @Override
     public List<Resume> getAllSorted() {
-        return helper.executeStatement(connectionFactory, "SELECT * FROM resume ORDER BY full_name, uuid", statement -> {
+        return helper.executeStatement("SELECT * FROM resume ORDER BY full_name, uuid", statement -> {
             ResultSet resultSet = statement.executeQuery();
             List<Resume> list = new ArrayList<>();
             while (resultSet.next()) {
@@ -90,7 +86,7 @@ public class SqlStorage implements Storage {
 
     @Override
     public int size() {
-        return helper.executeStatement(connectionFactory, "SELECT COUNT(*) FROM resume", statement -> {
+        return helper.executeStatement("SELECT COUNT(*) FROM resume", statement -> {
             ResultSet resultSet = statement.executeQuery();
             resultSet.next();
             return resultSet.getInt(1);
--- src/javawebinar/basejava/util/SqlHelper.java
+++ src/javawebinar/basejava/util/SqlHelper.java
@@ -1,5 +1,6 @@
 package javawebinar.basejava.util;
 
+import javawebinar.basejava.exception.ExistStorageException;
 import javawebinar.basejava.exception.StorageException;
 import javawebinar.basejava.sql.ConnectionFactory;
 
@@ -9,10 +10,19 @@ import java.sql.SQLException;
 
 public class SqlHelper {
 
-    public <T> T executeStatement(ConnectionFactory connectionFactory, String request, StatementProcessor<T> statementProcessor) {
+    private final ConnectionFactory connectionFactory;
+
+    public SqlHelper(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    public <T> T executeStatement(String request, StatementProcessor<T> statementProcessor) {
         try (Connection connection = connectionFactory.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(request)) {
             return statementProcessor.processStatement(preparedStatement);
         } catch (SQLException e) {
+            if (e.getMessage().contains("duplicate key value")) {
+                throw new ExistStorageException("uuid");
+            }
             throw new StorageException(e);
         }
     }
